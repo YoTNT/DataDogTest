ea9b7135b0d32e402f06ddda1d6dac8ff188a5a2
YoTNT <pikaqiujian@gmail.com>
2021-05-07 13:31:54 -0400

    add new API for testing ERROR log

--- LogFeeder/DatadogTestFeeder/src/main/java/dev/lnt/controllers/LogTestController.java
+++ LogFeeder/DatadogTestFeeder/src/main/java/dev/lnt/controllers/LogTestController.java
@@ -1,22 +1,44 @@
 package dev.lnt.controllers;
 
+import java.util.Date;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ch.qos.logback.classic.Logger;
 
 @RestController
 @RequestMapping("/logtests")
 @CrossOrigin("*")
 public class LogTestController {
-
+	
+	private static Logger logger = (Logger) LoggerFactory.getLogger(LogTestController.class);
+	
 	@GetMapping
 	public ResponseEntity<String> testLog(){
+		
+		logger.info("Accessing testLog controller at " + new Date());
+		
 		return ResponseEntity
 				.status(HttpStatus.OK)
 				.body("Access successfully!");
+	}
+	
+	@PostMapping
+	public ResponseEntity<String> testLogWithError(){
+		
+		logger.error("Simulate ERROR log from the backend at " + new Date());
+		
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body("Throw exception successfully!");
+		
 	}
 	
 }
