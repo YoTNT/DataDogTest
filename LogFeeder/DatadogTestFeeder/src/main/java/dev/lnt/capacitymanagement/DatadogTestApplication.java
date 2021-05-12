ba704d824ec3ad61a0a0832f868fe53f4fbcf097
YoTNT <pikaqiujian@gmail.com>
2021-05-12 09:58:33 -0400

    add StatsDClient for metrics to DataDog

--- LogFeeder/DatadogTestFeeder/src/main/java/dev/lnt/capacitymanagement/DatadogTestApplication.java
+++ LogFeeder/DatadogTestFeeder/src/main/java/dev/lnt/capacitymanagement/DatadogTestApplication.java
@@ -4,11 +4,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.timgroup.statsd.NonBlockingStatsDClientBuilder;
+import com.timgroup.statsd.StatsDClient;
+
 @SpringBootApplication
 @ComponentScan("dev.lnt")
 public class DatadogTestApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		
+		StatsDClient Statsd = new NonBlockingStatsDClientBuilder()
+									.prefix("statsd")
+									.hostname("localhost")
+									.port(8125)
+									.build();
+		
+		for(int i = 0; i < 10; i++) {
+			Statsd.incrementCounter("example_metric.increment", new String[] {"environment:dev"});
+			Statsd.decrementCounter("example_metric.decrement", new String[] {"environment:dev"});
+			Statsd.count("example_metric.count", 2, new String[] {"environment:dev"});
+			Thread.sleep(1000);
+		}
+		
 		SpringApplication.run(DatadogTestApplication.class, args);
 	}
 
